100e5517ee622b9f2d1e6cfd03f9ba7604bc416b
James <JamesPGrant1999@gmail.com>
2022-06-15 23:23:44 -0400

    test

--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ import aboutPage from './menu-pages/about'
 import foodItems from './menu-pages/food'
 import "./styles.css";
 import choc from '../src/images/choc.png'
+import cherry from  '../src/images/choc.png'
 const container = document.querySelector('.container')
 
+const myChoc = new Image();
+myChoc.src = choc
 
 function header(){
     const heading = createElement('h1', {class: 'heading'})
     heading.textContent = 'Doughnut Shop'
     return heading
+}
+
+function loadImage(){
+    const myChoc = new Image();
+    myChoc.src = choc
 }
 
 container.appendChild(header())
